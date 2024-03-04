f50761982f425a9b2726b10244373bfb799e9a8f
Sanidhya Sahu <98507281+sanidhya-sahu@users.noreply.github.com>
2024-03-04 21:26:26 +0530

    mailer service done

--- backend/services/mailer.js
+++ backend/services/mailer.js
@@ -21,7 +21,7 @@ const sendComplainEmail = async (complainId, wardenEmail,supervisorEmail, studen
         to: wardenEmail,
         cc:supervisorEmail,
         subject: `Complain regarding ${block}`,
-        text:`Respected Warden,\n\n ${studentName}, ${regid} has an complain reagrding ${block}.\n\nComplain : ${complainDesc}.\nComplain ID : ${complainId}.\nComplain Status : ${status} 
+        text:`Respected Warden,\n\n ${studentName},${regid} has an complain reagrding ${block}.\n\nComplain : ${complainDesc}.\nComplain ID : ${complainId}.\nComplain Status : ${status} 
             `
         
     };
@@ -34,6 +34,26 @@ const sendComplainEmail = async (complainId, wardenEmail,supervisorEmail, studen
             return console.log({ error })
         });
 }
-sendComplainEmail("complain_ID","aneesahu4@gmail.com","sanidhyasahu2022@vitbhopal.ac.in","Sanidhya sahu","22BAI10234","no water in block 1","Boys Hostel Block 1","raised")
 
-module.exports = {sendComplainEmail};
+const sendSuggetionEmail = async (studentName, regid, description,title) => {
+    let message = {
+        from: 'sanidhyasahu194@gmail.com',
+        to: "sanidhyasahu2022@vitbhopal.ac.in", // Suggestion respective faculty email
+        cc:supervisorEmail,
+        subject: title,
+        text:`Respected Sir,\n\n ${studentName},${regid} has an suggestion reagrding ${title}.\n\nDescription :${description}
+            `
+        
+    };
+
+    transporter.sendMail(message)
+        .then(() => {
+            return console.log({ msg: 'Email sent' })
+        })
+        .catch(error => {
+            return console.log({ error })
+        });
+}
+// sendComplainEmail("complain_ID","aneesahu4@gmail.com","sanidhyasahu2022@vitbhopal.ac.in","Sanidhya sahu","22BAI10234","no water in block 1","Boys Hostel Block 1","raised")
+
+module.exports = {sendComplainEmail,sendSuggetionEmail};
